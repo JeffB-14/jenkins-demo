9a8e7c58a75245142d5f6a9613baa1d95fb07b6a
Jeff Beier <jeff.beier@cygrp.com>
2022-01-21 15:09:37 -0600

    new branch to create the job for theme-park-rides

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/JeffB-14/theme_park_rides.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
